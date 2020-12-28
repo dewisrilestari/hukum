199770c27237c1692d4403d3b94c60fef2a4fe1b
dewisrilestari <201651099@std.umk.ac.id>
2020-12-28 20:55:34 +0700

    jdihkudus1

---
+++ www/db/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+	$host="localhost";
+	$username="adsn7114_jdih";
+	$password="123jdihkudus";
+	$database="adsn7114_jdih";
+	$conn=mysqli_connect($host,$username,$password,$database);
+?>
---
+++ www/home.php
@@ -0,0 +1,17 @@
+<?php
+include "db/koneksi.php";
+// $th=data("Y");
+	$sql="SELECT * FROM produk_hukum_old where thn=2019 ";
+	$hub=mysqli_query($conn,$sql);
+	while ( $data=mysqli_fetch_array($hub)) {
+		echo "        <div class='container' >
+            <div class='row'>
+            <div class='col-sm-12 block' style='background-color:#E6E6FA; color: black; '>
+            <p>Tentang :  " . $data['tentang'] ."</p>
+            </br>
+            <p>Download : </p>
+            </div>
+            </div>
+        </div></br>";
+	}
+?>
